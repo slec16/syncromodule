d954bd148ee7b597c8d123cd97b0fd91902f3964
Alexandr Kopytov <sankopytov.76@mail.ru>
2023-06-13 10:07:53 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,95 @@
+var interface = new Vue({
+    el: '#app',
+    data: {
+        sitename: 'Модуль синхронизации',
+        timestamp: '',
+        status: {
+            gate: '',
+            refsignal: '',
+        },
+        erorState: '',
+    },
+    created() {
+        setInterval(this.getNow, 1000);
+    },
+    methods: {
+        getNow: function() {
+            const today = new Date();
+            const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+            const dateTime = date +' '+ time;
+            this.timestamp = dateTime;
+        },
+        
+        submitValue() {
+            console.log(this.status.gate);
+            console.log(this.status.refsignal);
+            const CA = require('node-epics-ca');
+            (async () => {
+                try {
+                    let value;
+                    const pv = new CA.Channel('gate');
+                    await pv.connect();
+                    value = await pv.get();
+                    console.log(value);
+                    await pv.put(this.status.gate);
+                    value = await pv.get();
+                    console.log(value);
+                    await pv.disconnect()
+                } catch (error) {
+                    console.log(error);
+                }
+            })();
+            (async () => {
+                try {
+                    let value;
+                    const pv = new CA.Channel('freq');
+                    await pv.connect();
+                    value = await pv.get();
+                    console.log(value);
+                    await pv.put(this.status.refsignal);
+                    value = await pv.get();
+                    console.log(value);
+                    await pv.disconnect()
+                } catch (error) {
+                   console.log(error);
+                }
+            })()
+        },
+
+        checkValue() {
+            // this.status.gate = 4;
+            const CA = require('node-epics-ca').default;
+            
+            //let pv = new CA.Channel('gate');
+            (async () => {
+                try {
+                    let pv = new CA.Channel('gate');
+                    await pv.connect();
+                    let value = await pv.get();
+                    this.state.gate = value
+                    console.log(value);
+                    await pv.disconnect()
+                } catch (error) {
+                   console.log(error);
+                }
+            })();
+            //let pv1 = new CA.Channel('freq');
+            (async () => {
+                try {
+                    let pv1 = new CA.Channel('freq');
+                    await pv1.connect();
+                    let value1 = await pv1.get();
+                    this.state.refsignal = value1
+                    console.log(value1);
+                    await pv1.disconnect()
+                } catch (error) {
+                   console.log(error);
+                }
+            })()
+        }
+    },
+    
+
+})
+
